e157b6ef5cea21ad568e94ca1e01d883269b39dc
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-06-07 14:53:41 +0200

    styling
    made some styling changes and extracted the contents of the ArtSpace composable into three smaller composables + changed some of the string resources

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -5,10 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -20,10 +21,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -45,55 +50,92 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ArtSpace( modifier: Modifier = Modifier) {
     Column(
-        verticalArrangement = Arrangement.Bottom,
         horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween,
         modifier = modifier
-        .fillMaxSize()
+            .fillMaxSize()
+    ) {
+        ArtworkDisplay(modifier)
+
+        ArtworkSummary(modifier)
+
+        NavigationButtons(modifier)
+    }
+}
+
+@Composable
+private fun ArtworkDisplay(modifier: Modifier) {
+    Box(
+        modifier = modifier
+            .shadow(elevation = 3.dp)
     ) {
         Image(
             painterResource(id = R.drawable.hornbills),
             contentDescription = null,
             modifier = modifier
-                .width(300.dp)
-                .height(600.dp)
+                .padding(36.dp)
         )
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
+    }
+}
+
+@Composable
+private fun ArtworkSummary(modifier: Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .height(80.dp)
+            .fillMaxWidth()
+            .background(color = Color.LightGray)
+    ) {
+        Text(
+            stringResource(id = R.string.hornbills_title),
+            fontSize = 24.sp,
             modifier = modifier
-                .padding(29.dp)
-        ) {
-            Text(
-                stringResource(id = R.string.hornbills_title),
-                modifier = modifier,
-            )
-            Text(
-                stringResource(id = R.string.hornbills_artist_and_year),
-                modifier = modifier
-            )
-        }
+        )
         Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceAround,
-            modifier = modifier.fillMaxWidth()
+            horizontalArrangement = Arrangement.Center,
+            modifier = modifier
+                .padding(top = 10.dp)
         ) {
-            Button(
+            Text(
+                stringResource(id = R.string.hornbills_artist),
+                fontWeight = FontWeight.Bold,
                 modifier = modifier
-                    .width(110.dp),
-                onClick = { /*TODO*/ }
-            ) {
-                Text(text = "Previous")
-            }
-            Button(
+            )
+            Text(
+                stringResource(id = R.string.hornbills_year),
                 modifier = modifier
-                    .width(110.dp),
-                onClick = { /*TODO*/ }
-            ) {
-                Text(text = "Next")
-            }
+            )
         }
     }
 }
+
+@Composable
+private fun NavigationButtons(modifier: Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        Button(
+            modifier = modifier
+                .width(140.dp),
+            onClick = { /*TODO*/ }
+        ) {
+            Text(text = "Previous")
+        }
+        Button(
+            modifier = modifier
+                .width(140.dp),
+            onClick = { /*TODO*/ }
+        ) {
+            Text(text = "Next")
+        }
+    }
+}
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
